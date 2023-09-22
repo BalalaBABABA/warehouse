55d877539a63b2bce4690eb09427f4417df5791e
bala <bala@qq.com>
2023-09-22 11:01:39 +0800

    init_增加controller

--- src/main/java/com/abc/warehouse/controller/UserController.java
+++ src/main/java/com/abc/warehouse/controller/UserController.java
@@ -3,10 +3,7 @@ package com.abc.warehouse.controller;
 import com.abc.warehouse.dto.Result;
 import com.abc.warehouse.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
@@ -16,8 +13,8 @@ public class UserController {
     private UserService userService;
 
     @GetMapping("/{id}")
-    public Result getUserById(@RequestParam("id")Long id){
+    public Result getUserById(@PathVariable("id")Long id){
         // 调用userService的方法实现
-        return Result.ok();
+        return Result.ok("你好");
     }
 }
