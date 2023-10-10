dc54f0eb32d998f25a587e588de9933d05409ab6
bala <bala@qq.com>
2023-10-10 14:31:10 +0800

    用户增加

---
+++ src/main/java/com/abc/warehouse/pojo/User.java
@@ -0,0 +1,70 @@
+package com.abc.warehouse.pojo;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import java.io.Serializable;
+import java.util.Date;
+import lombok.Data;
+
+/**
+ * 人员表
+ * @TableName user_208201302
+ */
+@TableName(value ="user_208201302")
+@Data
+public class User implements Serializable {
+    /**
+     * 人员代码
+     */
+    private Long id;
+
+    /**
+     * 密码
+     */
+    private String password;
+
+    /**
+     * 姓名
+     */
+    private String name;
+
+    /**
+     * 角色
+     */
+    private String role;
+
+    /**
+     * 性别
+     */
+    private String sex;
+
+    /**
+     * 出生日期
+     */
+    private Date birthDate;
+
+    /**
+     * 身份证号
+     */
+    private String idNumber;
+
+    /**
+     * 籍贯
+     */
+    private String nativePlace;
+
+    /**
+     * 家庭住址
+     */
+    private String address;
+
+    /**
+     * 联系电话
+     */
+    private String phone;
+
+    @TableField(exist = false)
+    private static final long serialVersionUID = 1L;
+}
